23d00c3bd7a1e572686a5dae4712e796a73447eb
Tu Tu <fusornat@gmail.com>
2023-09-12 21:46:15 +0700

    add log,reg, get all product api

--- src/main/java/com/tuna/Controller/ProductController.java
+++ src/main/java/com/tuna/Controller/ProductController.java
@@ -1,83 +1,37 @@
-//package Controller;
-//
-//
-//import com.tuna.Model.Product;
-//import Model.ResponseObject;
-//import com.tuna.repositories.ProductRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//import java.util.Optional;
-//import org.springframework.web.bind.annotation.PutMapping;
-//
-//@RestController
-//@RequestMapping("/api/Product")
-//public class ProductController {
-//    @Autowired
-//    private ProductRepository productRepository;
-//
-//    // ... Other methods ...
-//
-////    @PutMapping("/{id}")
-////    public ResponseEntity<ResponseObject> updateProduct(@RequestBody Product newProduct, @PathVariable Long id) {
-////        return productRepository.findById(id)
-////                .map(product -> {
-////                    product.setProductName(newProduct.getProductName());
-////                    // Update other fields as needed
-////                    product.setYear(newProduct.getYear());
-////                    product.setPrice(newProduct.getPrice());
-////                    product.setUrl(newProduct.getUrl());
-////
-////                    try {
-////                        Product updatedProduct = productRepository.save(product);
-////                        return ResponseEntity.status(HttpStatus.OK).body(
-////                                new ResponseObject("Success", "Product updated", updatedProduct)
-////                        );
-////                    } catch (Exception e) {
-////                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-////                                new ResponseObject("Error", "Failed to update product", "")
-////                        );
-////                    }
-////                })
-////                .orElseGet(() -> {
-////                    newProduct.setId(id);
-////
-////                    try {
-////                        Product insertedProduct = productRepository.save(newProduct);
-////                        return ResponseEntity.status(HttpStatus.OK).body(
-////                                new ResponseObject("Success", "Product inserted", insertedProduct)
-////                        );
-////                    } catch (Exception e) {
-////                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-////                                new ResponseObject("Error", "Failed to insert product", "")
-////                        );
-////                    }
-////                });
-////    }
-////
-////    @DeleteMapping("/{id}")
-////    public ResponseEntity<ResponseObject> deleteProduct(@PathVariable Long id) {
-////        Optional<Product> foundProduct = productRepository.findById(id);
-////        if (foundProduct.isPresent()) {
-////            try {
-////                productRepository.deleteById(id);
-////                return ResponseEntity.status(HttpStatus.OK).body(
-////                        new ResponseObject("Success", "Product deleted", "")
-////                );
-////            } catch (Exception e) {
-////                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-////                        new ResponseObject("Error", "Failed to delete product", "")
-////                );
-////            }
-////        }
-////        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-////                new ResponseObject("Error", "Product not found", "")
-////        );
-////    }
-//}
+package com.tuna.Controller;
+
+import com.tuna.Model.Product;
+import com.tuna.repositories.ProductRepository;
+import com.tuna.repositories.response.ResponseObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/Product")
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/getAllProducts")
+    List<Product> getAllProducts() {
+        return productRepository.findAll(
+        );
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<ResponseObject> getProductById(@PathVariable String id) {
+        Optional<Product> foundProduct = productRepository.findById(id);
+        return  foundProduct.map(product -> new ResponseEntity<>(
+                        new ResponseObject("Success", "Product found", product), HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(
+                        new ResponseObject("Error", "Product not found", ""), HttpStatus.NOT_FOUND));
+    }
+}
--- src/main/java/com/tuna/Entity/Product.java
+++ src/main/java/com/tuna/Entity/Product.java
@@ -1,96 +1,105 @@
-//package com.tuna.Model;
-//
-//import jakarta.persistence.*;
-//
-//@Entity
-//public class Product {
-//    @Id
-//    private String id; // Trường ID kiểu String
-//
-//    @Column(nullable = false, unique = true, length = 100)
-//    private String productName;
-//
-//    @Column(length = 100)
-//    private String productType;
-//
-//    @Column(length = 100)
-//    private String productBrand;
-//
-//    @Column(length = 1000)
-//    private String productDescription;
-//
-//    private String productImageUrl;
-//
-//    private int productPrice; // Added productPrice field
-//
-//    public Product() {
-//    }
-//
-//    public Product(String id, String productName, String productType, String productBrand, String productDescription, String productImageUrl, int productPrice) {
-//        this.id = id;
-//        this.productName = productName;
-//        this.productType = productType;
-//        this.productBrand = productBrand;
-//        this.productDescription = productDescription;
-//        this.productImageUrl = productImageUrl;
-//        this.productPrice = productPrice; // Initialize productPrice
-//    }
-//
-//    // Getter and setter methods for all fields (generated automatically or write them manually)
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public String getProductName() {
-//        return productName;
-//    }
-//
-//    public void setProductName(String productName) {
-//        this.productName = productName;
-//    }
-//
-//    public String getProductType() {
-//        return productType;
-//    }
-//
-//    public void setProductType(String productType) {
-//        this.productType = productType;
-//    }
-//
-//    public String getProductBrand() {
-//        return productBrand;
-//    }
-//
-//    public void setProductBrand(String productBrand) {
-//        this.productBrand = productBrand;
-//    }
-//
-//    public String getProductDescription() {
-//        return productDescription;
-//    }
-//
-//    public void setProductDescription(String productDescription) {
-//        this.productDescription = productDescription;
-//    }
-//
-//    public String getProductImageUrl() {
-//        return productImageUrl;
-//    }
-//
-//    public void setProductImageUrl(String productImageUrl) {
-//        this.productImageUrl = productImageUrl;
-//    }
-//
-//    public int getProductPrice() {
-//        return productPrice;
-//    }
-//
-//    public void setProductPrice(int productPrice) {
-//        this.productPrice = productPrice;
-//    }
-//}
+package com.tuna.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sp")
+public class Product {
+    @Id
+    @Column(name = "MA_SP", length = 45)
+    private String id;
+    @Column(name = "TEN_SP", length = 255)
+    private String productName;
+    @Column(name = "MA_LOAISP", length = 255)
+    private String productType;
+    @Column(name = "MA_HANGSX", length = 255)
+    private String productBrand;
+    @Column(name = "MIEUTA_SP", length = 255)
+    private String productDescription;
+    @Column(name = "HINHANH_SP", length = 255)
+    private String productImage;
+    @Column(name = "GIA", length = 255)
+    private int productPrice;
+
+    public Product() {
+    }
+
+    public Product(String id, String productName, String productType, String productBrand, String productDescription, String productImage, int productPrice) {
+        this.id = id;
+        this.productName = productName;
+        this.productType = productType;
+        this.productBrand = productBrand;
+        this.productDescription = productDescription;
+        this.productImage = productImage;
+        this.productPrice = productPrice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductBrand() {
+        return productBrand;
+    }
+
+    public void setProductBrand(String productBrand) {
+        this.productBrand = productBrand;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", productName='" + productName + '\'' +
+                ", productType='" + productType + '\'' +
+                ", productBrand='" + productBrand + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", productImage='" + productImage + '\'' +
+                ", productPrice=" + productPrice +
+                '}';
+    }
+}
