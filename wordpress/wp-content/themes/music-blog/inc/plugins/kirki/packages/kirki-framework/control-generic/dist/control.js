6b5d26a482f1d8aec0c748b03696c927cacc1a0d
tuna <fusornat@gmail.com>
2024-06-10 17:32:40 +0700

    delete wordpress

--- wordpress/wp-content/themes/music-blog/inc/plugins/kirki/packages/kirki-framework/control-generic/dist/control.js
+++
@@ -1,2 +0,0 @@
-wp.customize.controlConstructor["kirki-generic"]=wp.customize.kirkiDynamicControl.extend({initKirkiControl:function(i){(i=i||this).container.find("input, textarea").on("change keyup paste click",(function(){i.setting.set(jQuery(this).val())}))}});
-//# sourceMappingURL=control.js.map
