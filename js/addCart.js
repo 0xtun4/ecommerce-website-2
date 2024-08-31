9678195d7b6044ba285cece4f4af132abce22037
Tuna <tunatdev@gmail.com>
2024-08-31 15:07:25 +0700

    destroy

--- js/addCart.js
+++
@@ -1,9 +0,0 @@
-function addCart(id) {
-    $.post("CartProcess.php?",{'productID':id}, function(data, status){
-      var result = data.split("-");
-  
-      $("#cart_amount").text(result[0]);
-    });
-    $('.toast').toast({delay:1850});
-    $('.toast').toast('show');
-  }
